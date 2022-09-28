628024d195eb7ab8d2038189d203c4b8dd8780f8
Eldar Mussagali <eldar.mussagali@nu.edu.kz>
2022-09-28 18:31:30 +0600

    redesign

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import styles from "../styles/Navbar.module.scss";
+import Image from "next/image";
+
+export const Navbar = () => {
+  const handleRequestClick = () => {
+    window.scrollTo(0, document.body.scrollHeight);
+  };
+
+  return (
+    <nav className={styles.nav}>
+      <Image src="/logo.svg" width={100} height={50} />
+      <div className={styles.request} onClick={handleRequestClick}>
+        Өтініш
+      </div>
+    </nav>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,49 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.scss';
-import { Form } from '../components/Form';
+import Head from "next/head";
+import styles from "../styles/Home.module.scss";
+import { Form } from "../components/Form";
+import Image from "next/image";
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>Қандастар</title>
-				<meta name="description" content="Qandastar"/>
-				<link rel="preconnect" href="https://fonts.googleapis.com"/>
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"/>
-				<link rel="icon" href="/favicon.ico"/>
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>Қандастар</title>
+        <meta name="description" content="Qandastar" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-			<main className={styles.main}>
-				<div className={styles.hero}>
-					<div className={styles.textContainer}>
-						<h1 className={styles.title}>Қандастарға арналған өтiнiш қалдыру web платформасы</h1>
-						<div>
-							<div>
-								Ресей үкіметінің зұлым шешімдерінен зардап шеккен қандастарымызды туған еліне қайтаруға арналған жоба.
-							</div>
-							<div>
-								Орыстар білсін - <span className={styles.highlight}>Біз, қазақ халқы, өз қандастарымызды озбырлыққа қалдырмаймыз!</span>
-							</div>
-							<div className={styles.cta}>
-								Төменде бекітілген сауалнамаға өз аты-жөнізді және басқа да ақпараттарды толтырыныз. Біз сізбен тез
-								арада хабарласуға тырысамыз!
-							</div>
-						</div>
-					</div>
-				</div>
-				<Form/>
-			</main>
-		</>
-	)
+      <main className={styles.main}>
+        <div className={styles.hero}>
+          <div className={styles.container}>
+            <h1 className={styles.title}>Құрметті Ресейдегі қандастар!</h1>
+            <p>
+              "Соңғы күндері көрші елде орын алып жатқан жағдайлар бізге де ауыр
+              тиіп жатыр. Мобилизация деген сұмдық нәрсе ғой.. Әсіресе Орынбор,
+              Омбы, Астрахан өңірлеріндегі майданға жүздеген қазақ
+              бауырларымызды да алып кетіп жатыр екен. Жасын да, кәрісін де..
+              Обал-ай.. Кеткендердің көбісі аман келмейтіні анық қой..
+              <br />
+              Сондықтан біз жігіттермен келген әскер жасындағы қазақтарға
+              Астана, Атыраудағы жатақханалардан орын мен тамағын беріп,
+              көмектесейік деп шештік. Елге қарай жетсе болды ғой..
+              <br />
+              Ағайындар, қазір сөз бен дау көп, бірақ көп дауласпай, өзіміздің
+              қарапайым қазақтарымызды құтқарып қалайықшы.. Осындай жағдайларда
+              бір-бірімізге қолдау көрсетпесек, қашан және кім көрсетеді.. Осы
+              жағдайдағы бауырларымыз болса, өтініш қалдырыңыздыр. Көмекшілерім
+              қажетті ақпаратты береді."
+            </p>
+            <h2 className={styles.author}>Айдын Рахимбаев</h2>
+          </div>
+        </div>
+        <Form />
+      </main>
+    </>
+  );
 }
